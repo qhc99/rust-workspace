147037f223f7687a3861c94144c9c88350adca2e
qhc <onlinecqh99@gmail.com>
2023-07-22 13:22:36 -0400

    add rayon par

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -2,9 +2,5 @@ mod mat_mul;
 mod rayon_bench;
 
 fn main() {
-    // mat_mul::mat_mul_profile_demo();
-    // let mut mat = vec![vec![1.; 3]; 3];
-    // let p1 = &mut mat as * mut Vec<Vec<f64>>;
-    // let p2 = &mut mat as * mut Vec<Vec<f64>>;
-    rayon_bench::bench_matmul_strassen();
+    mat_mul::mat_mul_profile_demo();
 }
--- playground/src/mat_mul.rs
+++ playground/src/mat_mul.rs
@@ -1,16 +1,52 @@
 use rand::Rng;
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 use std::borrow::BorrowMut;
+use std::ops::{Deref, DerefMut};
 use std::sync::{Arc, Mutex};
 use std::time::Instant;
 use std::{cmp, thread};
 
-#[derive(Clone, Copy)]
-struct NoCheckSync<T> {
+#[derive(Copy, Clone)]
+struct UnsafeMutPtr<T> {
     pub ptr: *mut T,
 }
 
-unsafe impl<T> Sync for NoCheckSync<T> {}
+impl<T> Deref for UnsafeMutPtr<T> {
+    type Target = T;
 
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.ptr }
+    }
+}
+
+impl<T> DerefMut for UnsafeMutPtr<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.ptr }
+    }
+}
+
+unsafe impl<T> Send for UnsafeMutPtr<T> {}
+
+unsafe impl<T> Sync for UnsafeMutPtr<T> {}
+
+#[derive(Copy, Clone)]
+struct UnsafePtr<T> {
+    pub ptr: *const T,
+}
+
+impl<T> Deref for UnsafePtr<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.ptr }
+    }
+}
+
+unsafe impl<T> Send for UnsafePtr<T> {}
+
+unsafe impl<T> Sync for UnsafePtr<T> {}
+
+#[derive(Debug)]
 struct MatBlock {
     r1: usize,
     c1: usize,
@@ -18,6 +54,7 @@ struct MatBlock {
     c2: usize,
 }
 
+#[derive(Debug)]
 struct MatPartition {
     size_block: usize,
     m: usize,
@@ -89,35 +126,13 @@ fn mul_mat_block(
 
     for i in 0..m3r.r2 - m3r.r1 {
         for j in 0..m3r.c2 - m3r.c1 {
+            if i + m3r.r1 >= m3.len() || j + m3r.c1 >= m3[0].len() {
+                panic!("catch m3r: {:?}.\n", m3r)
+            }
             m3[i + m3r.r1][j + m3r.c1] += cache[i][j];
         }
     }
 }
-
-// unsafe fn mul_mat_block_par(
-//     m1: NoCheckSync<Vec<Vec<f32>>>,
-//     m1r: &MatBlock,
-//     m2: NoCheckSync<Vec<Vec<f32>>>,
-//     m2r: &MatBlock,
-//     m3: NoCheckSync<Vec<Vec<f32>>>,
-//     m3r: &MatBlock,
-// ) {
-//     let mut cache: Vec<Vec<f32>> = vec![vec![0.; (*m3r).c2 - (*m3r).c1]; (*m3r).r2 - (*m3r).r1];
-//     for i in 0..(*m3r).r2 - (*m3r).r1 {
-//         for j in 0..(*m3r).c2 - (*m3r).c1 {
-//             for k in 0..(*m2r).r2 - (*m2r).r1 {
-//                 cache[i][j] += (*m1.ptr)[i + (*m1r).r1][k + (*m1r).c1]
-//                     * (*m2.ptr)[k + (*m2r).r1][j + (*m2r).c1];
-//             }
-//         }
-//     }
-
-//     for i in 0..(*m3r).r2 - (*m3r).r1 {
-//         for j in 0..(*m3r).c2 - (*m3r).c1 {
-//             (*m3.ptr)[i + (*m3r).r1][j + (*m3r).c1] += cache[i][j];
-//         }
-//     }
-// }
 
 pub fn naive_mat_mul(
     arr1: &mut Vec<Vec<f32>>,
@@ -153,9 +168,8 @@ fn locality_mat_mul(
     arr1: &mut Vec<Vec<f32>>,
     arr2: &mut Vec<Vec<f32>>,
     arr_res: &mut Vec<Vec<f32>>,
-    w_s: usize
+    w_s: usize,
 ) {
-
     let size_a: usize = arr1.len();
     let size_c: usize = arr2[0].len();
 
@@ -163,34 +177,11 @@ fn locality_mat_mul(
     let p2 = MatPartition::new(&arr2, w_s);
     let p3 = MatPartition::new(&arr_res, w_s);
 
-    let pp1 = &p1 as *const MatPartition;
-    let pp2 = &p2 as *const MatPartition;
-    let pp3 = &p3 as *const MatPartition;
-
     reset_mat(arr1, arr2);
 
     let start = Instant::now();
     for i in 0..p3.rows_idx {
-        for j in 0..p3.cols_idx {
-            let arr3_block = p3.at(i, j);
-            for p in 0..arr3_block.r2 - arr3_block.r1 {
-                for q in 0..arr3_block.c2 - arr3_block.c1 {
-                    arr_res[p + arr3_block.r1][q + arr3_block.c1] = 0.;
-                }
-            }
-            for k in 0..p1.cols_idx {
-                let arr1_block = p1.at(i, k);
-                let arr2_block = p2.at(k, j);
-                mul_mat_block(
-                    arr1,
-                    &arr1_block,
-                    arr2,
-                    &arr2_block,
-                    arr_res,
-                    &arr3_block,
-                );
-            }
-        }
+        locality_mat_mul_row(i, arr1, arr2, arr_res, &p1, &p2, &p3);
     }
     let duration = start.elapsed();
 
@@ -204,6 +195,108 @@ fn locality_mat_mul(
     println!("ans = {:}", ans);
 }
 
+fn locality_mat_mul_row(
+    i: usize,
+    arr1: &Vec<Vec<f32>>,
+    arr2: &Vec<Vec<f32>>,
+    arr_res: &mut Vec<Vec<f32>>,
+    p1: &MatPartition,
+    p2: &MatPartition,
+    p3: &MatPartition,
+) {
+    for j in 0..p3.cols_idx {
+        let arr3_block = p3.at(i, j);
+        for p in 0..arr3_block.r2 - arr3_block.r1 {
+            for q in 0..arr3_block.c2 - arr3_block.c1 {
+                if p + arr3_block.r1 >= arr_res.len() || q + arr3_block.c1 >= arr_res[0].len() {
+                    panic!(
+                        "@@catch arr3_block: {:?}, i:{}, j:{}, p3:{:?}\n",
+                        arr3_block, i, j, p3
+                    );
+                }
+                arr_res[p + arr3_block.r1][q + arr3_block.c1] = 0.;
+            }
+        }
+        for k in 0..p1.cols_idx {
+            let arr1_block = p1.at(i, k);
+            let arr2_block = p2.at(k, j);
+            mul_mat_block(arr1, &arr1_block, arr2, &arr2_block, arr_res, &arr3_block);
+        }
+    }
+}
+
+fn locality_mat_mul_row_unsafe(
+    i: usize,
+    arr1: usize,
+    arr2: usize,
+    arr_res: usize,
+    p1: usize,
+    p2: usize,
+    p3: usize,
+) {
+    unsafe {
+        let arr1 = &*(arr1 as *mut Vec<Vec<f32>>);
+        let arr2 = &*(arr2 as *mut Vec<Vec<f32>>);
+        let arr_res = &mut *(arr_res as *mut Vec<Vec<f32>>);
+
+        let p1 = &*(p1 as *const MatPartition);
+        let p2 = &*(p2 as *const MatPartition);
+        let p3 = &*(p3 as *const MatPartition);
+        locality_mat_mul_row(i, arr1, arr2, arr_res, p1, p2, p3);
+    }
+}
+
+fn locality_mat_mul_par(
+    arr1: &mut Vec<Vec<f32>>,
+    arr2: &mut Vec<Vec<f32>>,
+    arr_res: &mut Vec<Vec<f32>>,
+    w_s: usize,
+) {
+    let size_a: usize = arr1.len();
+    let size_c: usize = arr2[0].len();
+
+    let p1 = MatPartition::new(&arr1, w_s);
+    let p2 = MatPartition::new(&arr2, w_s);
+    let p3 = MatPartition::new(&arr_res, w_s);
+    let loop_size = p3.rows_idx;
+
+    reset_mat(arr1, arr2);
+
+    let arr1 = arr1 as *mut Vec<Vec<f32>> as usize;
+    let arr2 = arr2 as *mut Vec<Vec<f32>> as usize;
+    let arr3 = arr_res as *mut Vec<Vec<f32>> as usize;
+
+    let p1 = &p1 as *const MatPartition as usize;
+    let p2 = &p2 as *const MatPartition as usize;
+    let p3 = &p3 as *const MatPartition as usize;
+
+    let start = Instant::now();
+    
+    let data: Vec<Vec<usize>> = (0..loop_size)
+        .map(|v| -> Vec<usize> { vec![v, arr1, arr2, arr3, p1, p2, p3] })
+        .collect();
+    data.par_iter().for_each(|v| -> () {
+        let i = v[0];
+        let arr1 = v[1];
+        let arr2 = v[2];
+        let arr_res = v[3];
+        let p1 = v[4];
+        let p2 = v[5];
+        let p3 = v[6];
+        locality_mat_mul_row_unsafe(i, arr1, arr2, arr_res, p1, p2, p3);
+    });
+    let duration = start.elapsed();
+
+    let mut ans = 0.;
+    for i in 0..size_a {
+        for j in 0..size_c {
+            ans += arr_res[i][j] as f64;
+        }
+    }
+    println!("rayon space locality: time elapsed {:?}", duration);
+    println!("ans = {:}", ans);
+}
+
 #[allow(dead_code)]
 pub fn mat_mul_profile_demo() {
     const SIZE_A: usize = 1300;
@@ -213,46 +306,7 @@ pub fn mat_mul_profile_demo() {
     let mut arr2 = vec![vec![0.; SIZE_C]; SIZE_B];
     let mut arr_res = vec![vec![0.; SIZE_C]; SIZE_A];
 
-    //--------------------------------------------------------------
-    reset_mat(&mut arr1, &mut arr2);
-
-    let start = Instant::now();
-
-    thread::spawn(move || -> () {});
-
-    // for i in 0..p3.rows_idx {
-    //     for j in 0..p3.cols_idx {
-    //         let arr3_block = p3.at(i, j);
-    //         for p in 0..arr3_block.r2 - arr3_block.r1 {
-    //             for q in 0..arr3_block.c2 - arr3_block.c1 {
-    //                 arr_res[p + arr3_block.r1][q + arr3_block.c1] = 0.;
-    //             }
-    //         }
-    //         for k in 0..p1.cols_idx {
-    //             let arr1_block = p1.at(i, k);
-    //             let arr2_block = p2.at(k, j);
-
-    //             unsafe {
-    //                 mul_mat_block_par(
-    //                     &mut arr1 as *mut _,
-    //                     &arr1_block as *const _,
-    //                     &mut arr2 as *mut _,
-    //                     &arr2_block as *const _,
-    //                     &mut arr_res as *mut _,
-    //                     &arr3_block as *const _,
-    //                 )
-    //             }
-    //         }
-    //     }
-    // }
-    let duration = start.elapsed();
-
-    let mut ans = 0.;
-    for i in 0..SIZE_A {
-        for j in 0..SIZE_C {
-            ans += arr_res[i][j] as f64;
-        }
-    }
-    println!("space locality par: time elapsed {:?}", duration);
-    println!("ans = {:}", ans);
+    // naive_mat_mul(&mut arr1, &mut arr2, &mut arr_res);
+    // locality_mat_mul(&mut arr1, &mut arr2, &mut arr_res, 32);
+    locality_mat_mul_par(&mut arr1, &mut arr2, &mut arr_res, 64)
 }
