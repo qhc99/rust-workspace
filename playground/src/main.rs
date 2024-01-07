4a532f1eec1d6340d2d9b50152e2bf3a026eb03b
qhc <onlinecqh99@gmail.com>
2024-01-06 19:40:54 -0500

    Remove temp code

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,31 +1,7 @@
 #![allow(clippy::needless_range_loop)]
 #![allow(dead_code)]
-
-use mat_mul::mat_mul_profile_demo;
 mod mat_mul;
-use std::marker::PhantomData;
-
-struct Bounded<'a, 'b: 'a, T: ?Sized>(&'a T, PhantomData<&'b ()>);
-
-fn helper<'a, 'b, T: ?Sized>(
-    input: &'a T,
-    closure: impl FnOnce(&T) -> Bounded<'b, '_, T>,
-) -> &'b T {
-    closure(input).0
-}
-
-fn extend<'a, 'b, T: ?Sized>(input: &'a T) -> &'b T {
-    helper(input, |x| Bounded(x, PhantomData))
-}
-
-fn use_after_free(){
-    // mat_mul_profile_demo();
-    let s = String::from("aaaa");
-    let a: &'static str = extend(s.as_str()); // turn &'a str into 'static
-    drop(s);
-    println!("{}",a); // <------------ Use after free!
-}
-
-fn main() {
-    use_after_free()
+use crate::mat_mul::mat_mul_profile_demo;
+fn main(){
+    mat_mul_profile_demo();
 }
