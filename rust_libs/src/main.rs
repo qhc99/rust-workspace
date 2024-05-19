28968e1c61ee780c736fb1ae2eca2e0e281b3699
qhc <onlinecqh99@gmail.com>
2024-05-19 15:00:20 -0400

    Remove shared_list

--- rust_libs/src/main.rs
+++ rust_libs/src/main.rs
@@ -1,6 +1,5 @@
 #![allow(clippy::needless_return)]
 mod dlink_list;
 mod nullable_ptr;
-mod shared_list;
 
 fn main() {}
--- rust_libs/src/shared_list.rs
+++
@@ -1,7 +0,0 @@
-use std::rc::Rc;
-
-#[allow(dead_code)]
-struct SharedList<T> {
-    val: T,
-    parent: Option<Rc<SharedList<T>>>,
-}
