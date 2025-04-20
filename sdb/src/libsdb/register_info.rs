b04af8afd2d22a8bc08c7d6414e45723b006e44b
qhc99 <onlincqh99@gmail.com>
2025-04-19 21:44:39 -0400

    Fix build errors

--- sdb/src/libsdb/register_info.rs
+++ sdb/src/libsdb/register_info.rs
@@ -1,16 +1,12 @@
 use core::mem::offset_of;
 use core::mem::size_of;
+use std::fs;
+use std::path::Path;
 use libc::user;
-use libc::user_regs_struct;
 use libc::user_fpregs_struct;
+use libc::user_regs_struct;
 use nix::libc;
-use once_cell::sync::Lazy;
-use regex::Regex;
-
-
-enum RegisterId {}
-
-static REGS_INFO: &str = include_str!("../../resource/reg_info.txt");
+use register_codegen::generate_registers;
 
 macro_rules! gpr_offset {
     ($reg:ident) => {
@@ -24,111 +20,28 @@ macro_rules! fpr_offset {
     };
 }
 
+macro_rules! dr_offset {
+    ($number:ident) => {
+        offset_of!(user, u_debugreg) + number * 8
+    };
+}
+
 macro_rules! sizeof_field {
     ($struct:ty, $field:ident) => {{
         let uninit = std::mem::MaybeUninit::<$struct>::uninit();
         let base = uninit.as_ptr();
         let field = unsafe { &(*base).$field };
-        mem::size_of_val(field)
+        core::mem::size_of_val(field)
     }};
 }
 
 macro_rules! fpr_size {
     ($reg:ident) => {
-        sizeof_field!(user_fpregs_struct, reg)
+        sizeof_field!(user_fpregs_struct, $reg)
     };
 }
 
-static REG_TABLE: Lazy<Vec<(String, String, String, String, String, String)>> = Lazy::new(|| {
-    let mut res = Vec::<(String, String, String, String, String, String)>::new();
-    
-    let re = Regex::new(r"DEFINE_GPR_64\((.+?),(.+?)\)").expect("regex compilation failed");
-    re.captures_iter(REGS_INFO).for_each(|cap| {
-        let name = cap.get(1).unwrap().as_str().trim();
-        let dwarf_id = cap.get(2).unwrap().as_str().trim();
-        res.push((
-            name.to_string(),
-            dwarf_id.to_string(),
-            "8".to_string(),
-            format!("gpr_offset!({name})"),
-            "RegisterType::Gpr".to_string(),
-            "RegisterFormat::Uint".to_string(),
-        ));
-    });
-
-    let re = Regex::new(r"DEFINE_GPR_32\((.+?),(.+?)\)").expect("regex compilation failed");
-    re.captures_iter(REGS_INFO).for_each(|cap| {
-        let name = cap.get(1).unwrap().as_str().trim();
-        let super_ = cap.get(2).unwrap().as_str().trim();
-        res.push((
-            name.to_string(),
-            "-1".to_string(),
-            "4".to_string(),
-            format!("gpr_offset!({super_})"),
-            "RegisterType::SubGpr".to_string(),
-            "RegisterFormat::Uint".to_string(),
-        ));
-    });
-
-    let re = Regex::new(r"DEFINE_GPR_16\((.+?),(.+?)\)").expect("regex compilation failed");
-    re.captures_iter(REGS_INFO).for_each(|cap| {
-        let name = cap.get(1).unwrap().as_str().trim();
-        let super_ = cap.get(2).unwrap().as_str().trim();
-        res.push((
-            name.to_string(),
-            "-1".to_string(),
-            "2".to_string(),
-            format!("gpr_offset!({super_})"),
-            "RegisterType::SubGpr".to_string(),
-            "RegisterFormat::Uint".to_string(),
-        ));
-    });
-
-    let re = Regex::new(r"DEFINE_GPR_8H\((.+?),(.+?)\)").expect("regex compilation failed");
-    re.captures_iter(REGS_INFO).for_each(|cap| {
-        let name = cap.get(1).unwrap().as_str().trim();
-        let super_ = cap.get(2).unwrap().as_str().trim();
-        res.push((
-            name.to_string(),
-            "-1".to_string(),
-            "1".to_string(),
-            format!("gpr_offset!({super_})+1"),
-            "RegisterType::SubGpr".to_string(),
-            "RegisterFormat::Uint".to_string(),
-        ));
-    });
-
-    let re = Regex::new(r"DEFINE_GPR_8L\((.+?),(.+?)\)").expect("regex compilation failed");
-    re.captures_iter(REGS_INFO).for_each(|cap| {
-        let name = cap.get(1).unwrap().as_str().trim();
-        let super_ = cap.get(2).unwrap().as_str().trim();
-        res.push((
-            name.to_string(),
-            "-1".to_string(),
-            "1".to_string(),
-            format!("gpr_offset!({super_})"),
-            "RegisterType::SubGpr".to_string(),
-            "RegisterFormat::Uint".to_string(),
-        ));
-    });
-
-    let re = Regex::new(r"DEFINE_FPR\((.+?),(.+?),(.+?)\)").expect("regex compilation failed");
-    re.captures_iter(REGS_INFO).for_each(|cap| {
-        let name = cap.get(1).unwrap().as_str().trim();
-        let dwarf_id = cap.get(2).unwrap().as_str().trim();
-        let user_name = cap.get(2).unwrap().as_str().trim();
-        res.push((
-            name.to_string(),
-            dwarf_id.to_string(),
-            format!("fpr_size!({user_name})").to_string(),
-            format!("fpr_offset!({user_name})"),
-            "RegisterType::Fpr".to_string(),
-            "RegisterFormat::Uint".to_string(),
-        ));
-    });
-
-    return res;
-});
+generate_registers!("sdb/resource/reg_info.txt");
 
 enum RegisterType {
     Gpr, // General purpose register
@@ -146,7 +59,7 @@ enum RegisterFormat {
 
 struct RegisterInfo {
     id: RegisterId,
-    name: String,
+    name: &'static str,
     dwarf_id: i32,
     size: usize,
     offset: usize,
@@ -154,8 +67,3 @@ struct RegisterInfo {
     format: RegisterFormat,
 }
 
-static GRegisterInfos: &[RegisterInfo] = &[];
-
-fn t(){
-    
-}
---
+++ sdb/src/libsdb/registers_codegen/codegen.rs
@@ -0,0 +1,213 @@
+use std::{fs, path::Path};
+
+use core::mem::offset_of;
+use libc::user;
+use nix::libc;
+use proc_macro::TokenStream;
+use proc_macro2::Ident;
+use quote::{format_ident, quote};
+use regex::Regex;
+use syn::LitStr;
+
+#[proc_macro]
+pub fn generate_registers(input: TokenStream) -> TokenStream {
+
+    let arg = syn::parse_macro_input!(input as LitStr);
+    let file_path = arg.value();
+    let path = std::env::current_dir().unwrap();
+    let current_path = path.to_str().unwrap();
+    let content = fs::read_to_string(Path::new(&file_path))
+        .expect(format!("{current_path}, cannot read {file_path}").as_str());
+
+    let mut variants = Vec::new();
+    let mut infos = Vec::new();
+
+    let mut push = |name: Ident,
+                    dwarf_id: proc_macro2::TokenStream,
+                    size_expr: proc_macro2::TokenStream,
+                    offset_expr: proc_macro2::TokenStream,
+                    typ: proc_macro2::TokenStream,
+                    fmt: proc_macro2::TokenStream| {
+        variants.push(quote! { #name });
+        infos.push(quote! {
+            RegisterInfo {
+                id: RegisterId::#name,
+                name: stringify!(#name),
+                dwarf_id: #dwarf_id,
+                size: #size_expr,
+                offset: #offset_expr,
+                type_: #typ,
+                format: #fmt,
+            }
+        });
+    };
+
+    let re = Regex::new(r"DEFINE_GPR_64\((.+?),(.+?)\)").unwrap();
+    for cap in re.captures_iter(&content) {
+        let name = cap[1].trim();
+        let dwarf_id = cap[2].trim().parse::<i32>().unwrap();
+        let ident = format_ident!("{name}");
+        push(
+            ident.clone(),
+            quote! {#dwarf_id},
+            quote!(8),
+            quote!(gpr_offset!(#ident)),
+            quote!(RegisterType::Gpr),
+            quote!(RegisterFormat::Uint),
+        );
+    }
+
+    let re = Regex::new(r"DEFINE_GPR_32\((.+?),(.+?)\)").expect("regex compilation failed");
+    for cap in re.captures_iter(&content) {
+        let name = cap.get(1).unwrap().as_str().trim();
+        let super_ = cap.get(2).unwrap().as_str().trim();
+        let ident = format_ident!("{super_}");
+
+        push(
+            format_ident!("{name}"),
+            quote! {-1},
+            quote! {4},
+            quote!(gpr_offset!(#ident)),
+            quote!(RegisterType::SubGpr),
+            quote!(RegisterFormat::Uint),
+        );
+    }
+
+    let re = Regex::new(r"DEFINE_GPR_16\((.+?),(.+?)\)").expect("regex compilation failed");
+    for cap in re.captures_iter(&content) {
+        let name = cap.get(1).unwrap().as_str().trim();
+        let super_ = cap.get(2).unwrap().as_str().trim();
+        let ident = format_ident!("{super_}");
+        push(
+            format_ident!("{name}"),
+            quote! {-1},
+            quote! {2},
+            quote!(gpr_offset!(#ident)),
+            quote!(RegisterType::SubGpr),
+            quote!(RegisterFormat::Uint),
+        );
+    }
+
+    let re = Regex::new(r"DEFINE_GPR_8H\((.+?),(.+?)\)").expect("regex compilation failed");
+    for cap in re.captures_iter(&content) {
+        let name = cap.get(1).unwrap().as_str().trim();
+        let super_ = cap.get(2).unwrap().as_str().trim();
+        let ident = format_ident!("{super_}");
+
+        push(
+            format_ident!("{name}"),
+            quote! {-1},
+            quote! {1},
+            quote!(gpr_offset!(#ident) + 1),
+            quote!(RegisterType::SubGpr),
+            quote!(RegisterFormat::Uint),
+        );
+    }
+
+    let re = Regex::new(r"DEFINE_GPR_8L\((.+?),(.+?)\)").expect("regex compilation failed");
+    for cap in re.captures_iter(&content) {
+        let name = cap.get(1).unwrap().as_str().trim();
+        let super_ = cap.get(2).unwrap().as_str().trim();
+        let ident = format_ident!("{super_}");
+
+        push(
+            format_ident!("{name}"),
+            quote! {-1},
+            quote! {1},
+            quote!(gpr_offset!(#ident)),
+            quote!(RegisterType::SubGpr),
+            quote!(RegisterFormat::Uint),
+        );
+    }
+
+    let re = Regex::new(r"DEFINE_FPR\((.+?),(.+?),(.+?)\)").expect("regex compilation failed");
+    for cap in re.captures_iter(&content) {
+        let name = cap.get(1).unwrap().as_str().trim();
+        let dwarf_id = cap.get(2).unwrap().as_str().trim().parse::<i32>().unwrap();
+        let user_name = cap.get(3).unwrap().as_str().trim();
+        let ident = format_ident!("{user_name}");
+
+        push(
+            format_ident!("{name}"),
+            quote! {#dwarf_id},
+            quote!(fpr_size!(#ident)),
+            quote!(fpr_offset!(#ident)),
+            quote!(RegisterType::Fpr),
+            quote!(RegisterFormat::Uint),
+        );
+    }
+
+    let re = Regex::new(r"DEFINE_FP_ST\((.+?)\)").expect("regex compilation failed");
+    for cap in re.captures_iter(&content) {
+        let number = cap.get(1).unwrap().as_str().trim().parse::<usize>().unwrap();
+        let number_i32 = cap.get(1).unwrap().as_str().trim().parse::<i32>().unwrap();
+        let name = Box::leak(Box::new(format!("st{number}")));
+        let name = name.as_str();
+        push(
+            format_ident!("{name}"),
+            quote! {33+#number_i32},
+            quote! {16},
+            quote!(fpr_offset!(st_space)+#number*16),
+            quote!(RegisterType::Fpr),
+            quote!(RegisterFormat::LongDouble),
+        );
+    }
+
+    let re = Regex::new(r"DEFINE_FP_MM\((.+?)\)").expect("regex compilation failed");
+    for cap in re.captures_iter(&content) {
+        let number = cap.get(1).unwrap().as_str().trim().parse::<usize>().unwrap();
+        let number_i32 = cap.get(1).unwrap().as_str().trim().parse::<i32>().unwrap();
+        let name = format!("mm{number}");
+        let name = name.as_str();
+        push(
+            format_ident!("{name}"),
+            quote! {41+#number_i32},
+            quote! {8},
+            quote!(fpr_offset!(st_space)+#number*16),
+            quote!(RegisterType::Fpr),
+            quote!(RegisterFormat::Vector),
+        );
+    }
+
+    let re = Regex::new(r"DEFINE_FP_XMM\((.+?)\)").expect("regex compilation failed");
+    for cap in re.captures_iter(&content) {
+        let number = cap.get(1).unwrap().as_str().trim().parse::<usize>().unwrap();
+        let number_i32 = cap.get(1).unwrap().as_str().trim().parse::<i32>().unwrap();
+        let name = format!("xmm{number}");
+        let name = name.as_str();
+        push(
+            format_ident!("{name}"),
+            quote! {17+#number_i32},
+            quote! {16},
+            quote!(fpr_offset!(st_space)+#number*16),
+            quote!(RegisterType::Fpr),
+            quote!(RegisterFormat::Vector),
+        );
+    }
+
+    let re = Regex::new(r"DEFINE_DR\((.+?)\)").expect("regex compilation failed");
+    for cap in re.captures_iter(&content) {
+        let number = cap.get(1).unwrap().as_str().trim();
+        let num = number.parse::<usize>().unwrap();
+        let offset = offset_of!(user, u_debugreg) + num * 8;
+        let name = format!("dr{number}");
+        let name = name.as_str();
+        push(
+            format_ident!("{name}"),
+            quote! {-1},
+            quote! {8},
+            quote!(#offset),
+            quote!(RegisterType::Dr),
+            quote!(RegisterFormat::Uint),
+        );
+    }
+
+    let expanded = quote! {
+        enum RegisterId { #( #variants, )* }
+
+        static GRegisterInfos: &[RegisterInfo] = &[
+            #( #infos, )*
+        ];
+    };
+    TokenStream::from(expanded)
+}
