13be5521fb653a9c2115145734e4c400b3dae528
qhc99 <onlincqh99@gmail.com>
2025-04-30 16:04:44 -0400

    Add parse, virt addr

--- sdb/src/libsdb/parse.rs
+++ sdb/src/libsdb/parse.rs
@@ -1,8 +1,69 @@
+use regex::bytes::Regex;
+
 use super::register_info::RegisterFormat;
 use super::registers::F80;
 use super::sdb_error::SdbError;
 use super::{register_info::RegisterInfo, registers::RegisterValue};
 use std::str::FromStr;
+
+macro_rules! parse_int {
+    ($size:expr, $digits:expr, {
+        $($num:expr => $Variant:ident, $Ty:ty);+ $(;)?
+    }) => {
+        match $size {
+            $(
+                $num => Ok(RegisterValue::$Variant(
+                    <$Ty>::from_str_radix($digits, 16)
+                        .map_err(|_| SdbError::new("Invalid format"))?,
+                )),
+            )+
+            _ => SdbError::err("Invalid format"),
+        }
+    };
+}
+
+macro_rules! parse_vector {
+    ($size:expr, $text:expr, {
+        $($num:expr => $Variant:ident);+ $(;)?
+    }) => {
+        match $size {
+            $(
+                $num => {
+                    let re_pat = format!("\\[(0x\\w{}\\s*,\\s*){}\\]", 2, $num);
+                    let re = Regex::new(&re_pat).unwrap();
+                    let re_bytes = Regex::new(r"0x(\w{2})").unwrap();
+                    return if re.is_match($text.as_bytes()) {
+                        let parse_res: Vec<_> = re_bytes
+                            .captures(&$text.as_bytes())
+                            .unwrap()
+                            .iter()
+                            .map(|digits| {
+                                u8::from_str_radix(
+                                    std::str::from_utf8(digits.unwrap().as_bytes()).unwrap(),
+                                    16,
+                                )
+                            })
+                            .collect();
+                        if parse_res.iter().all(|data| data.is_ok()) {
+                            let bytes: [u8; $num] = parse_res
+                                .into_iter()
+                                .map(|data| data.unwrap())
+                                .collect::<Vec<_>>()
+                                .try_into()
+                                .unwrap();
+                            Ok(RegisterValue::$Variant(bytes))
+                        } else {
+                            SdbError::err("Invalid format")
+                        }
+                    } else {
+                        SdbError::err("Invalid format")
+                    };
+                }
+            )+
+            _ => SdbError::err("Invalid format"),
+        }
+    };
+}
 
 pub fn parse_register_value(info: &RegisterInfo, text: &str) -> Result<RegisterValue, SdbError> {
     match info.format {
@@ -12,21 +73,12 @@ pub fn parse_register_value(info: &RegisterInfo, text: &str) -> Result<RegisterV
             } else {
                 &text
             };
-            match info.size {
-                1 => Ok(RegisterValue::U8(
-                    u8::from_str_radix(digits, 16).map_err(|_| SdbError::new("Invalid format"))?,
-                )),
-                2 => Ok(RegisterValue::U16(
-                    u16::from_str_radix(digits, 16).map_err(|_| SdbError::new("Invalid format"))?,
-                )),
-                4 => Ok(RegisterValue::U32(
-                    u32::from_str_radix(digits, 16).map_err(|_| SdbError::new("Invalid format"))?,
-                )),
-                8 => Ok(RegisterValue::U64(
-                    u64::from_str_radix(digits, 16).map_err(|_| SdbError::new("Invalid format"))?,
-                )),
-                _ => SdbError::err("Invalid format"),
-            }
+            parse_int!(info.size, digits, {
+                1 => U8, u8;
+                2 => U16, u16;
+                4 => U32, u32;
+                8 => U64, u64;
+            })
         }
         RegisterFormat::DoubleFloat => Ok(RegisterValue::Double(
             f64::from_str(text).map_err(|_| SdbError::new("Invalid format"))?,
@@ -34,10 +86,9 @@ pub fn parse_register_value(info: &RegisterInfo, text: &str) -> Result<RegisterV
         RegisterFormat::LongDouble => Ok(RegisterValue::LongDouble(F80::new(
             f64::from_str(text).map_err(|_| SdbError::new("Invalid format"))?,
         ))),
-        RegisterFormat::Vector => match info.size {
-            8 => todo!(),
-            16 => todo!(),
-            _ => SdbError::err("Invalid format"),
-        },
+        RegisterFormat::Vector => parse_vector!(info.size, text, {
+            8 => Byte64;
+            16=> Byte128;
+        })
     }
 }
--- sdb/src/libsdb/types.rs
+++ sdb/src/libsdb/types.rs
@@ -1,2 +1,48 @@
+use std::ops::{Add, AddAssign, Sub, SubAssign};
+
 pub type Byte64 = [u8; 8];
 pub type Byte128 = [u8; 16];
+
+#[repr(transparent)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct VirtualAddress {
+    addr: u64,
+}
+
+impl VirtualAddress {
+    pub fn get_addr(&self) -> u64 {
+        self.addr
+    }
+}
+
+impl Add<i64> for VirtualAddress {
+    type Output = VirtualAddress;
+
+    fn add(self, rhs: i64) -> Self::Output {
+        Self {
+            addr: (self.addr as i128 + rhs as i128) as u64,
+        }
+    }
+}
+
+impl AddAssign<i64> for VirtualAddress {
+    fn add_assign(&mut self, rhs: i64) {
+        self.addr = (self.addr as i128 + rhs as i128) as u64;
+    }
+}
+
+impl Sub<i64> for VirtualAddress {
+    type Output = VirtualAddress;
+
+    fn sub(self, rhs: i64) -> Self::Output {
+        Self {
+            addr: (self.addr as i128 - rhs as i128) as u64,
+        }
+    }
+}
+
+impl SubAssign<i64> for VirtualAddress {
+    fn sub_assign(&mut self, rhs: i64) {
+        self.addr = (self.addr as i128 - rhs as i128) as u64;
+    }
+}
