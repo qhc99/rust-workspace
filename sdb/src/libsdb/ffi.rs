1d1d81ecf7cb5a0e0cec06a2a1204308c8c4f30d
qhc99 <onlincqh99@gmail.com>
2025-06-30 00:57:15 -0400

    Refactor

--- sdb/src/libsdb/ffi.rs
+++ sdb/src/libsdb/ffi.rs
@@ -1,39 +1,13 @@
-use nix::libc;
-
-use libc::size_t;
+use cpp_demangle::Symbol;
+use std::ffi::CStr;
 use std::os::raw::{c_char, c_int};
 
-#[link(name = "stdc++")] // Only support Linux os
-unsafe extern "C" {
-    fn __cxa_demangle(
-        mangled: *const c_char,
-        out_buf: *mut c_char,
-        out_len: *mut size_t,
-        status: *mut c_int,
-    ) -> *mut c_char;
-}
-
-use std::ffi::{CStr, CString};
-use std::ptr;
-
 pub fn demangle(name: &str) -> Option<String> {
-    let c_name = CString::new(name).ok()?;
-    let mut status = 0;
-    unsafe {
-        let demangled_ptr = __cxa_demangle(
-            c_name.as_ptr(),
-            ptr::null_mut(),
-            ptr::null_mut(),
-            &mut status,
-        );
-        if status == 0 && !demangled_ptr.is_null() {
-            let result = CStr::from_ptr(demangled_ptr).to_string_lossy().into_owned();
-            libc::free(demangled_ptr as *mut libc::c_void);
-            Some(result)
-        } else {
-            None
-        }
+    let sym = Symbol::new(name);
+    if let Ok(sym) = sym {
+        return Some(sym.to_string());
     }
+    None
 }
 
 unsafe extern "C" {
