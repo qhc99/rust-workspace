2304d233cd9e3f744048d61891f60729a877367d
qhc99 <onlincqh99@gmail.com>
2025-08-03 15:22:37 -0400

    Fix test

--- sdb/resource/member_pointer.cpp
+++ sdb/resource/member_pointer.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-
 struct cat {
     const char* name;
     void meow() const { std::cout << "meow\n"; }
 };
 int main() {
-    const char* (cat::*data_ptr) = &cat::name;
-    void (cat::*func_ptr)() const = &cat::meow;
+    const char* (cat:: * data_ptr) = &cat::name;
+    void (cat:: * func_ptr)() const = &cat::meow;
 
     cat marshmallow{ "Marshmallow" };
 
