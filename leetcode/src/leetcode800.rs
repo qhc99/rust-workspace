5db3d6189cbeb411f0a22f468c1fdb38a847884c
qhc99 <onlincqh99@gmail.com>
2025-08-16 16:12:36 -0400

    Add

--- leetcode/src/leetcode800.rs
+++ leetcode/src/leetcode800.rs
@@ -1,3 +1,5 @@
+use std::cmp::{max, min};
+
 /// #813
 pub fn largest_sum_of_averages(nums: Vec<i32>, k: i32) -> f64 {
     let n = nums.len();
@@ -278,6 +280,22 @@ pub fn sum_of_distances_in_tree(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
     children_sum(0, &tree, &descent_count, &mut sum);
     parent_sum(0, 0, 0, &tree, &descent_count, &mut sum);
     return sum;
+}
+
+/// #837
+pub fn new21_game(n: i32, k: i32, max_pts: i32) -> f64 {
+    let mut arr = vec![0f64; (n + 1) as usize];
+    let mut sum = vec![0f64; (n + 2) as usize];
+    let max_pts = max_pts as f64;
+    arr[0] = 1f64;
+    sum[1] = 1f64;
+    for i in 1..=min(n, k - 1 + max_pts as i32) {
+        let start = max(0, i - max_pts as i32) as usize;
+        let end = min(i, k) as usize;
+        arr[i as usize] = (sum[end] - sum[start]) / max_pts;
+        sum[i as usize + 1] = arr[i as usize] + sum[i as usize];
+    }
+    arr.iter().skip(k as usize).sum()
 }
 
 /// #838
